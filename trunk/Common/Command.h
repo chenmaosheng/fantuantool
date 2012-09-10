17717a9ede5336e3037415c540888d9f38a3e760
chanmaosheng@gmail.com <chanmaosheng@gmail.com@91d3ba80-2ded-617b-2aae-06650f87712b>
2012-09-10 15:25:00 +0000

    1, add new network library named starnet 2, add command instead of packet
    vet/app: me

---
+++ trunk/Common/Command.h
@@ -0,0 +1,115 @@
+#ifndef _H_COMMAND
+#define _H_COMMAND
+
+enum
+{
+	LOGIN = 1,
+	LOGOUT,
+	SEND_MESSAGE,
+};
+
+template<unsigned short cmdId>
+class Command
+{
+public:
+	static const unsigned short ID = cmdId;
+
+	Command()
+	{
+		m_iCmdId = ID;
+	}
+
+	virtual ~Command()
+	{
+	}
+
+public:
+	int		m_iCmdId;
+};
+
+class LoginCommand : public Command<LOGIN>
+{
+public:
+	LoginCommand()
+	{
+	}
+
+	~LoginCommand()
+	{
+	}
+
+public:
+};
+
+class LogoutCommand : public Command<LOGOUT>
+{
+public:
+	LogoutCommand()
+	{
+	}
+
+	~LogoutCommand()
+	{
+	}
+
+public:
+};
+
+class SendMessageCommand : public Command<SEND_MESSAGE>
+{
+public:
+	SendMessageCommand()
+	{
+	}
+
+	~SendMessageCommand()
+	{
+	}
+
+public:
+};
+
+struct Header
+{
+	char type;
+	int  len;
+
+	Header()
+	{
+		len = 0;
+	}
+};
+
+struct LoginPkt : public Header
+{
+	LoginPkt()
+	{
+		type = LOGIN;
+		memset(nickname, 0, sizeof(nickname));
+		index = 0;
+	}
+	int index;
+	char nickname[32];
+};
+
+struct LogoutPkt : public Header
+{
+	LogoutPkt()
+	{
+		type = LOGOUT;
+		index = 0;
+	}
+	int index;
+};
+
+struct SendMessagePkt : public Header
+{
+	SendMessagePkt()
+	{
+		type = SEND_MESSAGE;
+		memset(message, 0, sizeof(message));
+	}
+	char message[256];
+};
+
+#endif

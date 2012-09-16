d1f4fed09102d889cf134093c779404a9570d164
chanmaosheng@gmail.com <chanmaosheng@gmail.com@91d3ba80-2ded-617b-2aae-06650f87712b>
2012-09-16 16:44:53 +0000

    try to change all of actions to asynchronized, not finished, hard working! temporarily guarantee no compile error
    vet/app: me

--- trunk/Server/connection.h
+++ trunk/Server/connection.h
@@ -1,25 +1,36 @@
 #ifndef _H_CONNECTION
 #define _H_CONNECTION
 
-#include "context.h"
 #include "handler.h"
 
 typedef HANDLE	ConnID;
 
+struct Context;
+class ContextPool;
+class Worker;
+class Acceptor;
 struct Connection : SLIST_ENTRY
 {
 	SOCKET			socket_;
-	SOCKADDR_IN		sockAddr_;
-	Context			sendContext_;
-	Context			recvContext_;
-	LONG			connected_;
+	SOCKADDR_IN		sockaddr_;
 	Handler			handler_;
+	ContextPool*	context_pool_;
+	Worker*			worker_;
+	Acceptor*		acceptor_;
+	Context*		context_;
+	void*			client_;
 	
-	Connection();
+	LONG			iorefs_;
+	LONG			connected_;
+	LONG			iorefmax_;
+	
+	int32 Connect(PSOCKADDR_IN addr, void* client);
+	void Disconnect();
+	void AsyncSend(Context*);
+	void AsyncRecv(Context*);
 
-	void AsyncRecv();
-
-	void AsyncSend(int len, char* buf);
+	static Connection* Create(Handler* pHandler, ContextPool* pContextPool, Worker* pWorker, Acceptor* pAcceptor);
+	static void Close(Connection*);
 };
 
 #endif
--- trunk/Server/worker.cpp
+++ trunk/Server/worker.cpp
@@ -2,6 +2,8 @@
 #include "connection.h"
 #include "context.h"
 #include <process.h>
+#include "acceptor.h"
+#include "context_pool.h"
 
 Worker::Worker()
 {
@@ -48,48 +50,128 @@ uint32 WINAPI Worker::WorkerThread(PVOID pParam)
 {
 	BOOL bResult;
 	DWORD dwNumRead;
-	Connection* pConnection = NULL;
 	LPOVERLAPPED lpOverlapped = NULL;
 	Context* pContext = NULL;
+	ULONG_PTR key;
+	Connection* pConnection = NULL;
+	Acceptor* pAcceptor = NULL;
 
 	Worker* pWorker = (Worker*)pParam;
 
-	while (true)
+	do
 	{
-		bResult = GetQueuedCompletionStatus(pWorker->iocp_, &dwNumRead, (ULONG_PTR*)&pConnection, &lpOverlapped, INFINITE);
+		bResult = GetQueuedCompletionStatus(pWorker->iocp_, &dwNumRead, &key, &lpOverlapped, INFINITE);
 		if (lpOverlapped)
 		{
 			pContext = (Context*)((char*)lpOverlapped - CTXOFFSET);
+			pConnection = pContext->connection_;
 			switch(pContext->operation_type_)
 			{
+			case OPERATION_ACCEPT:
+				{
+					pAcceptor = pConnection->acceptor_;
+					if (bResult)
+					{
+						int32 rc = 0;
+						pAcceptor->Accept();
+						rc = setsockopt(pConnection->socket_, SOL_SOCKET, SO_UPDATE_ACCEPT_CONTEXT, (const char*)&pAcceptor->socket_, sizeof(pAcceptor->socket_));
+						if (rc == 0)
+						{
+							pConnection->connected_ = 1;
+							if (pConnection->handler_.OnConnection((ConnID)pConnection) && 
+								(pContext = pConnection->context_pool_->PopInputContext()))
+							{
+								pConnection->AsyncRecv(pContext);
+							}
+							else
+							{
+								pConnection->Disconnect();
+							}
+						}
+
+						InterlockedDecrement(&pAcceptor->iorefs_);
+					}
+				}
+				break;
+
+			case OPERATION_CONNECT:
+				{
+					if (bResult)
+					{
+						int32 rc = 0;
+						
+						pConnection->connected_ = 1;
+						rc = setsockopt(pConnection->socket_, SOL_SOCKET, SO_UPDATE_CONNECT_CONTEXT, NULL, 0);
+						if (rc == 0)
+						{
+							if (pConnection->handler_.OnConnection((ConnID)pConnection) &&
+								(pContext = pConnection->context_pool_->PopInputContext()))
+							{
+								pConnection->AsyncRecv(pContext);
+							}
+							else
+							{
+								pConnection->Disconnect();
+							}
+						}
+					}
+					else
+					{
+						pConnection->handler_.OnConnectFailed(pConnection->client_);
+						Connection::Close(pConnection);
+					}
+				}
+				break;
+
+			case OPERATION_DISCONNECT:
+				{
+					if (pConnection->iorefs_)
+					{
+						PostQueuedCompletionStatus(pWorker->iocp_, dwNumRead, key, lpOverlapped);
+					}
+					else
+					{
+						pConnection->handler_.OnDisconnect((ConnID)pConnection);
+					}
+				}
+				break;
+
 			case OPERATION_RECV:
 				{
 					if (dwNumRead == 0)
 					{
-						printf("client %s disconnected\n", inet_ntoa(pConnection->sockAddr_.sin_addr));
-						pConnection->handler_.OnDisconnect((ConnID)pConnection);
+						printf("client %s disconnected\n", inet_ntoa(pConnection->sockaddr_.sin_addr));
+						pConnection->context_pool_->PushInputContext(pContext);
+						pConnection->Disconnect();
 					}
 					else
 					{
-						printf("client %s send something\n", inet_ntoa(pConnection->sockAddr_.sin_addr));
+						printf("client %s send something\n", inet_ntoa(pConnection->sockaddr_.sin_addr));
 						
 						pConnection->handler_.OnData((ConnID)pConnection, (uint16)dwNumRead, pContext->buffer_);
-						pConnection->AsyncRecv();
+						pConnection->AsyncRecv(pContext);
 					}
 					
+					InterlockedDecrement(&pConnection->iorefs_);
 				}
 				break;
 
 			case OPERATION_SEND:
 				{
-					
+					pConnection->context_pool_->PushOutputContext(pContext);
+					InterlockedDecrement(&pConnection->iorefs_);
 				}
 				break;
 			default:
 				break;
 			}
 		}
+		else
+		{
+			break;
+		}
+	}while(true);
 
-	}
+	return 0;
 }
 

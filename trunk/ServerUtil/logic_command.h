841277799a41f75f2e4e745d847bb7ad6029f81b
chanmaosheng@gmail.com <chanmaosheng@gmail.com@91d3ba80-2ded-617b-2aae-06650f87712b>
2012-09-19 16:44:45 +0000

    1, add todo list 2, implement logiccommand and logicloop, not finished
    vet/app: me

--- trunk/ServerUtil/logic_command.h
+++ trunk/ServerUtil/logic_command.h
@@ -3,6 +3,13 @@
 
 #include "common.h"
 
+enum
+{
+	COMMAND_ONCONNECT,
+	COMMAND_ONDISCONNECT,
+	COMMAND_ONDATA,
+};
+
 template<uint16 iCmdId>
 class LogicCommand
 {
@@ -18,6 +25,43 @@ public:
 
 public:
 	int		m_iCmdId;
+};
+
+class LogicCommandOnConnect : public LogicCommand<COMMAND_ONCONNECT>
+{
+public:
+	LogicCommandOnConnect()
+	{
+		m_ConnId = NULL;
+	}
+
+public:
+	ConnID m_ConnId;
+};
+
+class LogicCommandOnDisconnect : public LogicCommand<COMMAND_ONDISCONNECT>
+{
+public:
+	LogicCommandOnDisconnect()
+	{
+		m_ConnId = NULL;
+	}
+
+public:
+	ConnID m_ConnId;
+};
+
+class LogicCommandOnData : public LogicCommand<COMMAND_ONDATA>
+{
+public:
+	LogicCommandOnData();
+	~LogicCommandOnData();
+	int32 CopyData(uint16 iLen, const char* pData);
+	
+public:
+	ConnID m_ConnId;
+	uint16 m_iLen;
+	char* m_pData;
 };
 
 #endif

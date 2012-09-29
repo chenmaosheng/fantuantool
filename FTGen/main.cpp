0b52e2a7d3f3a3a6bd42138c58447b1abef5ab57
chanmaosheng <chanmaosheng@gmail.com>
2012-09-29 10:13:50 +0000

    1, do some parser work, not finished 2, fix a log color bug
    vet/app: me

--- FTGen/main.cpp
+++ FTGen/main.cpp
@@ -1,270 +1,279 @@
 #include <cstdio>
 #include <fstream>
 #include <sstream>
+#include "ft_parser.h"
 
 const char* g_Type[] = {"int32", "int8", "uint8", "cpint8"};
 char g_Space[] = {' ', '\t', '\n', '\r'};
+NodeSet nodeSet;
 
-struct Node
+const char* escapeSpace(const char* buffer)
 {
-	char returnType[32];
-	char funcName[32];
-	struct Param
+	for (;; buffer++)
 	{
-		char paramType[32];
-		char paramName[32];
-	}paramSet[32];
-	int paramCount;
-};
-
-struct NodeSet
-{
-	NodeSet() { nodeCount = 0; includeCount = 0; }
-	Node node[32];
-	int nodeCount;
-	char includeFile[32][128];
-	int includeCount;
-};
-
-const char* CheckStr(const char* buffer, const char* strToCheck, const char*& nextToken)
-{
-	if(strstr(buffer, strToCheck) != buffer)
-	{
-		nextToken = buffer;
-		return NULL;
-	}
-	nextToken = buffer + strlen(strToCheck);
-	return nextToken;
-}
-
-const char* CheckType(const char* buffer, const char*& nextToken)
-{
-	for (size_t i = 0; i < sizeof(g_Type)/sizeof(g_Type[0]); ++i)
-	{
-		nextToken = CheckStr(buffer, g_Type[i], nextToken);
-		if (nextToken)
+		if (buffer[0] == '/' && buffer[1] == '/')
 		{
-			return g_Type[i];
-		}
-	}
-
-	return NULL;
-}
-
-const char* escapeSpace(const char* buffer, const char*& nextToken)
-{
-	bool bHit = false;
-	while (true)
-	{
-		bHit = false;
-		for (size_t i = 0; i < sizeof(g_Space)/sizeof(g_Space[0]); ++i)
-		{
-			if (buffer[0] == g_Space[i])
+			buffer+=2;
+			while (buffer[0] != '\0' && buffer[0] != '\n')
 			{
-				buffer+=1;
-				bHit = true;
-				break;
+				buffer++;
 			}
 		}
 
-		if (!bHit || buffer[0] == '\0')
+		if (buffer[0] == '\0')
 		{
-			nextToken = buffer;
 			return buffer;
 		}
-	}
 
-	//assert(false);
-	return NULL;
-}
+		if (buffer[0] == ' ' || buffer[0] == '\n')
+		{
+			continue;
+		}
 
-const char* escapeSpecial(char specialChar, const char* buffer, const char*& nextToken)
-{
-	if (buffer[0] == specialChar)
-	{
-		buffer+=1;
-		nextToken = buffer;
 		return buffer;
 	}
-
-	return NULL;
 }
 
-const char* CheckIdentifier(const char* buffer, const char*& nextToken)
+const char* CheckChar(const char* buffer, char c)
 {
-	const char* identifier = buffer;
-
-	while (true)
+	buffer = escapeSpace(buffer);
+	if (buffer[0] != c)
 	{
-		if ((buffer[0] >= 'a' && buffer[0] <= 'z') ||
-			(buffer[0] >= 'A' && buffer[0] <= 'Z') ||
-			(buffer[0] >= '0' && buffer[0] <= '9'))
+		return NULL;
+	}
+	return buffer+1;
+}
+
+const char* CheckKeyword(const char* buffer, char* value, int size)
+{
+	int index = 0;
+	buffer = escapeSpace(buffer);
+	for (;; index++)
+	{
+		if (buffer[index] >= '0' && buffer[index] <= '9')
 		{
-			buffer+=1;
+			if (index == 0)
+			{
+				return NULL;
+			}
+			continue;
 		}
-		else
+
+		if ((buffer[index] >= 'a' && buffer[index] <= 'z') ||
+			(buffer[index] >= 'A' && buffer[index] <= 'Z') || buffer[index] == '_')
+		{
+			continue;
+		}
+
+		break;
+	}
+
+	if (index == 0 || index + 1 > size)
+	{
+		return NULL;
+	}
+
+	memcpy(value, buffer, index);
+	value[index] = '\0';
+	return buffer+index;
+}
+
+const char* CheckString(const char* buffer, char* value, int size)
+{
+	int index = 0;
+	buffer = escapeSpace(buffer);
+	if (buffer[0] != '"')
+	{
+		return NULL;
+	}
+	for (index = 1;; index++)
+	{
+		if (buffer[index] == '\0')
+		{
+			return NULL;
+		}
+
+		if (buffer[index] == '"')
 		{
 			break;
 		}
 	}
+	if (index + 1 > size)
+	{
+		return NULL;
+	}
 
-	nextToken = buffer;
-	return identifier == buffer ? NULL : identifier;
+	memcpy(value, buffer+1, index);
+	value[index-1] = '\0';
+	return buffer+index+1;
 }
 
-const char* CheckParamList(const char* buffer, const char*& nextToken, Node::Param* pParamSet, int& paramCount)
+const char* CheckIdentifier(const char* buffer, const char* identifier, char* value)
 {
-	paramCount = 0;
-	do
+	char str[128] = {0};
+	buffer = escapeSpace(buffer);
+	buffer = CheckKeyword(buffer, str, sizeof(str));
+	if (!buffer)
 	{
-		const char* type = CheckType(buffer, nextToken);
-		if (!type)
-		{
-			break;
-		}
+		return NULL;
+	}
 
-		strcpy_s(pParamSet[paramCount].paramType, sizeof(pParamSet[paramCount].paramType), type);
-		pParamSet[paramCount].paramType[strlen(pParamSet[paramCount].paramType)+1] = '\0';
+	if (strcmp(str, identifier)!=0)
+	{
+		return NULL;
+	}
 
-		if (!escapeSpace(nextToken, nextToken))
-		{
-			break;
-		}
+	if (value)
+	{
+		strcpy(value, identifier);
+	}
+	return buffer;
+}
 
-		int length = 0;
-		const char* identifier = CheckIdentifier(nextToken, nextToken);
-		if (!identifier)
-		{
-			break;
-		}
+const char* CheckParamList(const char* buffer, Node* node)
+{
+	char name[128] = {0};
+	buffer = CheckKeyword(buffer, name, sizeof(name));
+	if (!buffer)
+	{
+		return NULL;
+	}
+	
+	strcpy(node->paramSet[node->paramCount].paramType, name);
+	node->paramSet[node->paramCount].paramType[strlen(node->paramSet[node->paramCount].paramType)+1] = '\0';
 
-		length = nextToken - identifier;
-		strncpy(pParamSet[paramCount].paramName, identifier, length);
-		pParamSet[paramCount].paramName[length+1] = '\0';
+	buffer = CheckKeyword(buffer, name, sizeof(name));
+	if (!buffer)
+	{
+		return NULL;
+	}
 
-		escapeSpecial(',', nextToken, nextToken);
+	strcpy(node->paramSet[node->paramCount].paramName, name);
+	node->paramSet[node->paramCount].paramName[strlen(node->paramSet[node->paramCount].paramName)+1] = '\0';
+	if (!buffer)
+	{
+		return NULL;
+	}
 
-		escapeSpace(nextToken, nextToken);
-
-		buffer = nextToken;
-
-		paramCount++;
-
-	}while (buffer[0] != ')');
+	node->paramCount++;
 
 	return buffer;
 }
 
-const char* CheckComment(const char* buffer, const char*& nextToken)
+const char* CheckInclude(const char* buffer)
 {
-	const char* identifier = buffer;
-
-	if (buffer[0] == '/' && buffer[1] == '/')
+	char file[128] = {0};
+	buffer = CheckChar(buffer, '#');
+	if (!buffer)
 	{
-		buffer+=2;
-		while (buffer[0] != '\0' && buffer[0] != '\n')
+		return NULL;
+	}
+	buffer = CheckIdentifier(buffer, "include", NULL);
+	if (!buffer)
+	{
+		return NULL;
+	}
+	buffer = CheckString(buffer, file, sizeof(file));
+	if (!buffer)
+	{
+		return NULL;
+	}
+
+	strcpy(nodeSet.includeFile[nodeSet.includeCount++], file);
+	return buffer;
+}
+
+const char* CheckFunction(const char* buffer)
+{
+	char name[128] = {0};
+	buffer = CheckKeyword(buffer, name, sizeof(name));
+	if (!buffer)
+	{
+		return NULL;
+	}
+	buffer = CheckChar(buffer, '(');
+	if (!buffer)
+	{
+		return NULL;
+	}
+	strcpy(nodeSet.node[nodeSet.nodeCount].returnType, name);
+	while (true)
+	{
+		const char* nextToken = CheckChar(buffer, ')');
+		if (!nextToken)
 		{
-			buffer++;
+			return NULL;
+		}
+
+		if (nodeSet.node[nodeSet.nodeCount].paramCount > 0)
+		{
+			buffer = CheckChar(buffer, ',');
+			if (!buffer)
+			{
+				return NULL;
+			}
+		}
+
+		buffer = CheckParamList(buffer, &nodeSet.node[nodeSet.nodeCount]);
+		if (!buffer)
+		{
+			return NULL;
 		}
 	}
 
-	nextToken = buffer;
-	return identifier == buffer ? NULL : buffer;
-}
-
-const char* CheckInclude(const char* buffer, const char*& nextToken)
-{
-	const char* identifier = buffer;
-	if (buffer[0] == '#')
+	buffer = CheckChar(buffer, ')');
+	if (!buffer)
 	{
-		buffer++;
-		while (buffer[0] != '\0' && buffer[0] != '\n')
-		{
-			buffer++;
-		}
+		return NULL;
+	}
+	buffer = CheckChar(buffer, ';');
+	if (!buffer)
+	{
+		return NULL;
 	}
 
-	nextToken = buffer;
-	return identifier == buffer ? NULL : identifier;
+	nodeSet.nodeCount++;
+	return buffer;
 }
 
-bool ParserFt(const char* buffer, NodeSet* nodeSet)
+const char* CheckAllFunctions(const char* buffer)
 {
-	const char* nextToken = buffer;
+	while (true)
+	{
+		const char* nextToken = CheckFunction(buffer);
+		if (!nextToken)
+		{
+			break;
+		}
+		buffer = nextToken;
+	}
+
+	return buffer;
+}
+
+bool ParserFt(const char* buf, NodeSet* nodeSet)
+{
+	const char* buffer = escapeSpace(buf);
 	Node* node = nodeSet->node;
-	while (nextToken[0] != '\0')
+	while (true)
 	{
-		while (true)
+		const char* nextToken;
+		nextToken = CheckInclude(buffer);
+		if (nextToken)
 		{
-			int length = 0;
-			const char* identifier = CheckInclude(nextToken, nextToken);
-			if (!identifier)
-			{
-				break;
-			}
-
-			length = nextToken - identifier;
-			strncpy(nodeSet->includeFile[nodeSet->includeCount], identifier, length);
-			nodeSet->includeFile[nodeSet->includeCount][length+1] = '\0';
-
-			escapeSpace(nextToken, nextToken);
-			nodeSet->includeCount++;
+			buffer = nextToken;
+			continue;
 		}
 
-		while (true)
+		nextToken = CheckAllFunctions(buffer);
+		if (nextToken)
 		{
-			const char* identifier = CheckComment(nextToken, nextToken);
-			if (!identifier)
-			{
-				break;
-			}
-			
-			escapeSpace(nextToken, nextToken);
+			buffer = nextToken;
+			continue;
 		}
 
-		const char* type = CheckType(nextToken, nextToken);
-		if (type)
-		{
-			strcpy_s(node[nodeSet->nodeCount].returnType, sizeof(node[nodeSet->nodeCount].returnType), type);
-			node[nodeSet->nodeCount].returnType[strlen(node[nodeSet->nodeCount].returnType)+1] = '\0';
-		}
-
-		if (!escapeSpace(nextToken, nextToken))
-		{
-		}
-
-		int length = 0;
-		const char* funcName = CheckIdentifier(nextToken, nextToken);
-		if (funcName)
-		{
-			length = nextToken - funcName;
-			strncpy(node[nodeSet->nodeCount].funcName, funcName, length);
-			node[nodeSet->nodeCount].funcName[length+1] = '\0';
-		}
-
-		escapeSpace(nextToken, nextToken);
-
-		if (!escapeSpecial('(', nextToken, nextToken))
-		{
-		}
-
-		if (!CheckParamList(nextToken, nextToken, node[nodeSet->nodeCount].paramSet, node[nodeSet->nodeCount].paramCount))
-		{
-		}
-
-		if(!escapeSpecial(')', nextToken, nextToken))
-		{
-		}
-
-		escapeSpace(nextToken, nextToken);
-
-		if(!escapeSpecial(';', nextToken, nextToken))
-		{
-		}
-
-		escapeSpace(nextToken, nextToken);
-
-		++nodeSet->nodeCount;
+		buffer = escapeSpace(buffer);
+		break;
 	}
 
 	return true;
@@ -277,7 +286,6 @@ int main(int argc, char* argv[])
 	str << stream.rdbuf();
 	std::string buf = str.str();
 	const char* buffer = buf.c_str();
-	NodeSet nodeSet;
 	ParserFt(buffer, &nodeSet);
 
 	return 0;
